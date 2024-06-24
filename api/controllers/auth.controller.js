639c8c94f3f01122a4b2514b4b9eda154aca3488
nikumbhShubham <shubhamnikumbh19@gmail.com>
2024-06-25 03:00:53 +0530

    add a middleware and a function to handle middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-export const signup = async (request, response) => {
+import { errorHandler } from '../utils/error.js'
+export const signup = async (request, response,next) => {
     const { username, email, password } = request.body
     const hashpassword=bcryptjs.hashSync(password,10)
     const newUser = new User({ username, email, password:hashpassword })
@@ -8,7 +9,8 @@ export const signup = async (request, response) => {
         await newUser.save()
         response.status(200).send("user created successfully!!")
     } catch (error) {
-        response.status(500).json(error.message)
+        // next(errorHandler(300,"something went wrong!!"))
+        next(error)
     }
 
 }
--- api/index.js
+++ api/index.js
@@ -3,16 +3,17 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
+import { error } from 'console'
 dotenv.config()
 
 
 
 mongoose.connect(process.env.MONGODB)
-.then(()=>{
-    console.log("connected to MongoDB");
-}).catch((err)=>{
-    console.log("Error connecting to MongoDB",err);
-})
+    .then(() => {
+        console.log("connected to MongoDB");
+    }).catch((err) => {
+        console.log("Error connecting to MongoDB", err);
+    })
 
 const app = express()
 app.use(express.json())
@@ -20,6 +21,21 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
 
-app.use('/api/user',userRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoute)
 
-app.use('/api/auth',authRoute)
+app.use((err, req, res, next) => {   //has to be in correct order ---> (err, req, res, next)
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal server error'
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+    // return res.status(statusCode).json({
+    //     success: false,
+    //     message,
+    //     statusCode
+    // })
+})
+
