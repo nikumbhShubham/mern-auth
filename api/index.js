ba9e7ab87f927e21c9701aeb436ce6a2a2f29e26
nikumbhShubham <shubhamnikumbh19@gmail.com>
2024-06-26 20:19:05 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config()
 
 
@@ -15,7 +16,16 @@ mongoose.connect(process.env.MONGODB)
         console.log("Error connecting to MongoDB", err);
     })
 
+const __dirname=path.resolve();
+
+
 const app = express()
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(request,response)=>{
+    response.sendFile(path.join(__dirname,'client','dist','index.html'))
+});
 app.use(express.json())
 // app.use(urlencoded({extended:false}))
 app.use(cookieParser())
